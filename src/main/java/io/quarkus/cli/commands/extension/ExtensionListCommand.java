12e94417f9c8dbf933f2f4ac251d16bf24dfe8ac
George Gastaldi <gegastaldi@gmail.com>
2020-06-01 12:23:06 -0300

    Implemented extension list

--- src/main/java/io/quarkus/cli/commands/extension/ExtensionListCommand.java
+++ src/main/java/io/quarkus/cli/commands/extension/ExtensionListCommand.java
@@ -1,7 +1,9 @@
 package io.quarkus.cli.commands.extension;
 
 import java.util.Comparator;
-import java.util.Set;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import javax.inject.Inject;
 
@@ -14,12 +16,23 @@ public class ExtensionListCommand implements Runnable {
 
     private static final String CONCISE_FORMAT = "%-50s %-50s";
 
+    @CommandLine.Unmatched
+    List<String> keyword;
+
     @Inject
     Registry registry;
 
     @Override
     public void run() {
+        final Predicate<Extension> filter;
+        if (keyword == null || keyword.isEmpty() || keyword.get(0).equals("*")) {
+            filter = extension -> true;
+        } else {
+            final Pattern searchPattern = Pattern.compile(".*" + keyword.get(0) + ".*", Pattern.CASE_INSENSITIVE);
+            filter = extension -> searchPattern.matcher(extension.getName()).matches();
+        }
         registry.getExtensions().stream().sorted(Comparator.comparing(Extension::getName))
+                .filter(filter)
                 .map( extension -> String.format(CONCISE_FORMAT, extension.getName(), extension.getId().getGroupArtifactId()))
                 .forEach(System.out::println);
     }
