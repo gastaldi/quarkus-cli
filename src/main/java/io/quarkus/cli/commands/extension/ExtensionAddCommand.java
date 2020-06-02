bc9df49008e604f800c95b866c7b36f9422acc80
George Gastaldi <gegastaldi@gmail.com>
2020-06-02 00:08:32 -0300

    Using parameter instead of option

--- src/main/java/io/quarkus/cli/commands/extension/ExtensionAddCommand.java
+++ src/main/java/io/quarkus/cli/commands/extension/ExtensionAddCommand.java
@@ -7,11 +7,11 @@ import picocli.CommandLine;
 @CommandLine.Command(name = "add", description = "Add an extension to the build descriptor")
 public class ExtensionAddCommand implements Runnable {
 
-    @CommandLine.Option(names = {"-e","--extension"}, completionCandidates = ExtensionIdCompletionCandidates.class)
+    @CommandLine.Parameters(completionCandidates = ExtensionIdCompletionCandidates.class)
     String extension;
 
     @Override
     public void run() {
-        Printer.ok(" Add extension "+ extension);
+        Printer.ok(" Add extension " + extension);
     }
 }
