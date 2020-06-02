86625a86ba5a3c6a2d74ff231780800bfc44792e
George Gastaldi <gegastaldi@gmail.com>
2020-06-01 21:52:32 -0300

    Cache

--- src/main/java/io/quarkus/cli/RegistryProducer.java
+++ src/main/java/io/quarkus/cli/RegistryProducer.java
@@ -1,10 +1,16 @@
 package io.quarkus.cli;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.cli.commands.registry.RegistryClient;
 import io.quarkus.cli.config.Config;
 import io.quarkus.extensions.catalog.model.registry.Registry;
@@ -16,18 +22,28 @@ public class RegistryProducer {
 
     @Produces
     @ApplicationScoped
-    Registry setUp(Config config) {
-        RegistryBuilder builder = new RegistryBuilder();
-        // TODO: Fetch from URL only if not cached locally previously
-        for (URI uri : config.getRegistries()) {
-            RegistryClient client = RestClientBuilder.newBuilder().baseUri(uri).build(RegistryClient.class);
-            Registry aRegistry = client.getRegistry();
-            builder.addAllCategories(aRegistry.getCategories())
-                    .addAllExtensions(aRegistry.getExtensions())
-                    .addAllPlatforms(aRegistry.getPlatforms())
-                    .addAllVersions(aRegistry.getVersions());
+    Registry setUp(Config config, ObjectMapper objectMapper) throws IOException {
+        final Registry registry;
+        Path path = Paths.get(System.getProperty("user.home"), ".quarkus", "registry.cache");
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        if (Files.exists(path)) {
+            registry = objectMapper.readValue(path.toFile(), Registry.class);
+        } else {
+            RegistryBuilder builder = new RegistryBuilder();
+            for (URI uri : config.getRegistries()) {
+                RegistryClient client = RestClientBuilder.newBuilder()
+                        .baseUri(uri).build(RegistryClient.class);
+                Registry aRegistry = client.getRegistry();
+                builder.addAllCategories(aRegistry.getCategories())
+                        .addAllExtensions(aRegistry.getExtensions())
+                        .addAllPlatforms(aRegistry.getPlatforms())
+                        .addAllVersions(aRegistry.getVersions());
+            }
+            registry = builder.build();
+            Files.createDirectories(path.getParent());
+            objectMapper.writeValue(path.toFile(), registry);
         }
-        return builder.build();
+        return registry;
     }
 
 }
